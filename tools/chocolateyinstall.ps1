5ff0f40eece69fd2244905936d0ef19984b33d77
Christian Decker <christian.decker@homag.com>
2023-10-19 07:20:06 +0200

    release opentofu 1.6.0-alpha3

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$url = 'https://github.com/opentofu/opentofu/releases/download/v1.6.0-alpha2/tofu_1.6.0-alpha2_windows_386.zip'
-$url64 = 'https://github.com/opentofu/opentofu/releases/download/v1.6.0-alpha2/tofu_1.6.0-alpha2_windows_amd64.zip'
-$checksum = 'C69F8FCECC1CD447677D2E143C41311C4A6D63F639A0504C23C6260D57CB8228'
-$checksum64 = '3F9EDE88A2233BE5221766BE460D191A33FF1E9C1BDAEF7920576D3838533523'
+$url = 'https://github.com/opentofu/opentofu/releases/download/v1.6.0-alpha3/tofu_1.6.0-alpha3_windows_386.zip'
+$url64 = 'https://github.com/opentofu/opentofu/releases/download/v1.6.0-alpha3/tofu_1.6.0-alpha3_windows_amd64.zip'
+$checksum = 'd0f4a5ff9dca2804a5c6d947cf234bc2604b8e4b5a798a710d964a019b44b1e9'
+$checksum64 = '395e7278136c06fe87ad4115ad5781332f37790adc5e5e64160b01d2a3acd53b'
 
 $unzipLocation = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
