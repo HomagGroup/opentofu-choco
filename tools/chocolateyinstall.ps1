daba663aecbcea0ad8792980ba9ad8260b331367
Christian Decker <christian.decker@homag.com>
2023-12-05 08:32:12 +0100

    release opentofu 1.6.0-beta2

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$url = 'https://github.com/opentofu/opentofu/releases/download/v1.6.0-alpha4/tofu_1.6.0-alpha4_windows_386.zip'
-$url64 = 'https://github.com/opentofu/opentofu/releases/download/v1.6.0-alpha4/tofu_1.6.0-alpha4_windows_amd64.zip'
-$checksum = '577ace2d25ce19565f4e649486d0eae399e6317f9d9cd28e27d034336a33d3e5'
-$checksum64 = 'b883638d6a571810e1b7cf16a2a46257517a8af786aeb25b53c5f388970a3b59'
+$url = 'https://github.com/opentofu/opentofu/releases/download/v1.6.0-beta2/tofu_1.6.0-beta2_windows_386.zip'
+$url64 = 'https://github.com/opentofu/opentofu/releases/download/v1.6.0-beta2/tofu_1.6.0-beta2_windows_amd64.zip'
+$checksum = '03a0dcb538ba4cdd67e6514eec99a79a40c2f8768403cc1fa0ac6a52c511a95e'
+$checksum64 = '9e3422bf6c8a6064fec342de743aae1dea26294e9339e3388eb87061593244ab'
 
 $unzipLocation = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
